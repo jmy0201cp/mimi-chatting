a44352d4790f2226647d794e3818c55449d451f0
miyoung <jmy0201cp@gmail.com>
2023-04-18 12:42:14 +0900

    added router pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,20 @@
 import "./App.css";
 import LoginPage from "./pages/LoginPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./pages/Signup";
+import ChatRoom from "./component/ChatRoom";
 
 export default function App() {
-  const token = localStorage.getItem("token");
+  // const token = localStorage.getItem("token");
 
-  return <>{!token && <LoginPage />}</>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/auth/login" element={<LoginPage />} />
+        <Route path="/auth/signup" element={<Signup />} />
+        <Route path="/chat/room" element={<ChatRoom />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
