d1a99a6223d4f9ef78217581d4efeb480c311ad1
miyoung <jmy0201cp@gmail.com>
2023-04-30 14:01:02 +0700

    replace user's data to mongoose.

--- server/repository/user.js
+++ server/repository/user.js
@@ -1,22 +1,17 @@
 import express from "express";
-import { v4 as uuidv4 } from "uuid";
+import mongoose, { Schema } from "mongoose";
 
-const secret = "flkdjseir12453ljdfaojdfnDFEns";
-//abc123: $2b$12$AcAcn3skiYOS6CC8jOw7Ke93ufxbdczAW9b3HFY4qlg193WHTO2Za
-let users = [
-  {
-    id: "123",
-    username: "camilla",
-    password: "$2b$12$AcAcn3skiYOS6CC8jOw7Ke93ufxbdczAW9b3HFY4qlg193WHTO2Za",
-  },
-];
+const userSchema = new Schema({
+  username: { type: String },
+  password: { type: String },
+});
+
+const User = mongoose.model("User", userSchema);
 
 export async function findUserByName(username) {
-  return users.find((user) => user.username === username);
+  return User.findOne({ username });
 }
 
 export async function signup(user) {
-  const created = { ...user, id: uuidv4() };
-  users = [...users, created];
-  return created.id;
+  return new User(user).save();
 }
