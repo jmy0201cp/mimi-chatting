8c08d9916fb197e1f781ad697a9e685a94de783c
miyoung <jmy0201cp@gmail.com>
2023-04-30 13:59:38 +0700

    add chat default route

--- server/router/chat.js
+++ server/router/chat.js
@@ -3,8 +3,15 @@ import * as chatRepository from "../repository/chat.js";
 
 const router = express.Router();
 
-router.get("/room", async (req, res) => {
+router.get("/", async (req, res) => {
   const list = await chatRepository.getAllList();
+  res.status(201).json(list);
+});
+
+router.get("/room", async (req, res) => {
+  console.log(req.body);
+  const list = await chatRepository.getChatUserList();
+  console.log(list);
   res.status(201).json(list);
 });
 
